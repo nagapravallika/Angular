caeb27efc15079a625a5f98d697935cc82df1ee0
nagapravallika <nagapravallikaraja@gmail.com>
2019-12-03 13:17:55 +0530

    json

---
+++ angular-json-crud/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {AppRoutingModule} from '../app/app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { EmployeesModule } from './modules/employees/employees.module';
+import {HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    EmployeesModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+  
+})
+export class AppModule { }
---
+++ angular-json-crud/src/app/model/employee.model.ts
@@ -0,0 +1,10 @@
+export class Employee
+{
+             id:number;
+             firstName:String;
+             lastName:String;
+             designation:String;
+             contact:number;
+             address:String;
+
+}
